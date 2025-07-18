e1e7bb12f785eb78c20e40acaf31c4fa55995158
Alkis <alkisax@gmail.com>
2025-07-18 11:24:58 +0300

    style: wip: dashbord sidebar

--- frontend/src/components/LeftSidebarDashboard.jsx
+++ frontend/src/components/LeftSidebarDashboard.jsx
@@ -1,35 +1,53 @@
+import {useNavigate} from "react-router-dom";
 
 const LeftSidebarDashboard = () => {
 
+  const navigate = useNavigate()
+  const navigateToPosts = () => {
+    navigate("/posts")
+  }
+
+  const navigateToUploads = () => {
+    navigate("/uploads")
+  }
+
   return (
     <div 
-      class="
+      className="
         bg-blue-100 text-blue-900
         p-4
         flex flex-row flex-wrap sm:flex-col
         sm:h-full
         gap-2
         w-full
+        overflow-hidden
       "
     >
-      <button className="px-4 py-2 bg-blue-600 text-white rounded sm:w-full whitespace-nowrap">
+      <button
+        onClick={navigateToPosts}
+        className="px-4 py-2 bg-blue-600 text-white rounded sm:w-full flex-shrink-0 text-sm"
+      >
         Posts
       </button>
-      <button className="px-4 py-2 bg-blue-600 text-white rounded sm:w-full whitespace-nowrap">
-        Posts2
+      <button 
+        onClick={navigateToUploads}
+        className="px-4 py-2 bg-blue-600 text-white rounded sm:w-full flex-shrink-0 text-sm"
+      >
+        Uploaded Files
       </button>
-      <button className="px-4 py-2 bg-blue-600 text-white rounded sm:w-full whitespace-nowrap">
-        Posts256
-      </button>
-      <button className="px-4 py-2 bg-blue-600 text-white rounded sm:w-full whitespace-nowrap">
-        Posts
-      </button>
-      <button className="px-4 py-2 bg-blue-600 text-white rounded sm:w-full whitespace-nowrap">
-        Posts2
-      </button>
-      <button className="px-4 py-2 bg-blue-600 text-white rounded sm:w-full whitespace-nowrap">
-        Posts256
-      </button>
+
+      <br />
+      <hr />
+      <strong>post</strong>
+      <hr />
+
+
+      <br />
+      <strong>testing text</strong>
+      <hr />
+      <p>
+        !Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi minus illum nisi est? At quisquam id nulla molestias delectus, rerum quas provident illo corrupti dolor minus, sint vero obcaecati incidunt?    
+      </p>
     </div>
   )
 }
